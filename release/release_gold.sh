189ff9d495ba9667551143e1728545d69e5f979b
Lucas Miranda <lucas.miranda@liferay.com>
2024-12-20 15:08:37 -0300

    LPD-44252 Check if LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH is equal to yes

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -135,7 +135,7 @@ function prepare_branch_to_commit {
 }
 
 function prepare_next_release_branch {
-	if [[ "${_PRODUCT_VERSION}" != *q* ]]
+	if [[ "${_PRODUCT_VERSION}" != *q* ]] || [ ! $(echo "${LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH}" | grep -i "yes") ]
 	then
 		lc_log INFO "Skipping the preparation of the next release branch."
 
@@ -350,7 +350,8 @@ function test_boms {
 function update_release_info_date {
 	if [[ "${_PRODUCT_VERSION}" != *q* ]] ||
 	   [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '.' -f 3)" -eq 0 ]] ||
-	   [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '.' -f 1)" -lt 2024 ]]
+	   [[ "$(echo "${_PRODUCT_VERSION}" | cut -d '.' -f 1)" -lt 2024 ]] ||
+	   [ ! $(echo "${LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH}" | grep -i "yes") ]
 	then
 		lc_log INFO "Skipping the release info update."
 
