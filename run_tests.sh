51bc5464b2dea2f749ac2176ead9d20ae6e9edbd
Davi Santos <davi.santos4357@gmail.com>
2025-08-08 11:11:24 -0300

    LPD-61489 Modify run_tests.sh to run only relevant test based on changed files
    - Compare the current branch with upstream/master to detect changed files.
    - Run only the relevant test sets (Docker or Release) based on those changes.
    - If no changes are found, run all tests.
    - If changes are detected in both Docker and Release scripts, run all tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 function main {
-	local tests_results=$(\
-		find . \( -name "test_build_*.sh" -o -name "test_patching_tool_version.sh" \) -type f -exec ./{} \; && \
-		\
-		cd release && \
-		\
-		find . -name "test_*.sh" -type f -exec ./{} \;)
+	local changed_files=$(git diff --name-only upstream/master)
+	local tests_results=""
+
+	if [ -z "${changed_files}" ]
+	then
+		tests_results=$(\
+			find . -maxdepth 1 \( -name "test_*.sh" ! -name "test_bundle_image.sh" \) -type f -exec ./{} \; && \
+			\
+			cd release && \
+			\
+			find . -name "test_*.sh" -type f -exec ./{} \;)
+	else
+		if (echo "${changed_files}" | grep --extended-regexp "^[^/]+\.sh$" --quiet)
+		then
+			tests_results=$(find . -maxdepth 1 -name "test_*.sh" ! -name "test_bundle_image.sh" -type f -exec ./{} \;)
+		fi
+
+		if (echo "${changed_files}" | grep --extended-regexp "^release/.*\.sh$|^release/test-dependencies/.*" --quiet)
+		then
+			tests_results+=$'\n'"$(cd release && find . -name "test_*.sh" -type f -exec ./{} \;)"
+		fi
+	fi
 
 	echo "${tests_results}"
 
