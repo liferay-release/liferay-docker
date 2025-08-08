856331088087a12e9b17b03d3f550d9fcaf273eb
Davi Santos <davi.santos4357@gmail.com>
2025-08-08 14:05:16 -0300

    LPD-61489 Rename

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,11 @@
 
 function main {
 	local changed_files=$(git diff --name-only upstream/master)
-	local tests_results=""
+	local test_results=""
 
 	if [ -z "${changed_files}" ]
 	then
-		tests_results=$(\
+		test_results=$(\
 			find . -maxdepth 1 \( -name "test_*.sh" ! -name "test_bundle_image.sh" \) -type f -exec ./{} \; && \
 			\
 			cd release && \
@@ -15,18 +15,18 @@ function main {
 	else
 		if (echo "${changed_files}" | grep --extended-regexp "^[^/]+\.sh$" --quiet)
 		then
-			tests_results=$(find . -maxdepth 1 -name "test_*.sh" ! -name "test_bundle_image.sh" -type f -exec ./{} \;)
+			test_results=$(find . -maxdepth 1 -name "test_*.sh" ! -name "test_bundle_image.sh" -type f -exec ./{} \;)
 		fi
 
 		if (echo "${changed_files}" | grep --extended-regexp "^release/.*\.sh$|^release/test-dependencies/.*" --quiet)
 		then
-			tests_results+=$'\n'"$(cd release && find . -name "test_*.sh" -type f -exec ./{} \;)"
+			test_results+=$'\n'"$(cd release && find . -name "test_*.sh" -type f -exec ./{} \;)"
 		fi
 	fi
 
-	echo "${tests_results}"
+	echo "${test_results}"
 
-	if [[ "${tests_results}" == *"FAILED"* ]]
+	if [[ "${test_results}" == *"FAILED"* ]]
 	then
 		exit 1
 	fi
