a6d13ae5a6982975d20cfb2e2ae39132c697c2de
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-08-24 16:10:50 -0500

    LPD-103623 Log the Caddy configuration as JSON on startup

--- templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
+++ templates/caddy/resources/usr/local/bin/liferay_caddy_entrypoint.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+function log_configuration {
+	local checksum
+	local file_name
+	local file_type
+
+	checksum=$(sha256sum /etc/caddy/Caddyfile | sed --expression "s/ .*//")
+
+	_log_json "Liferay Caddy configuration /etc/caddy/Caddyfile has checksum ${checksum}."
+
+	for file_name in /etc/caddy.d/*
+	do
+		if [ ! -e "${file_name}" ]
+		then
+			continue
+		fi
+
+		checksum=$(sha256sum "${file_name}" | sed --expression "s/ .*//")
+
+		file_type="unmanaged"
+
+		if [ "${file_name}" == "/etc/caddy.d/liferay_caddy_file" ]
+		then
+			file_type="generated"
+		fi
+
+		_log_json "Liferay Caddy configuration import ${file_name} is ${file_type} and has checksum ${checksum}."
+	done
+}
+
 function main {
 	if [ ! -n "${LIFERAY_ROUTES_DXP}" ]
 	then
@@ -12,6 +41,8 @@ function main {
 	do
 		local url="${protocol}://${i}"
 
+		_log_json "Liferay Caddy is allowing cross-origin requests from ${url}."
+
 		cat >> /etc/caddy.d/liferay_caddy_file << EOF
 @origin${url} header Origin ${url}
 header @origin${url} Access-Control-Allow-Origin "${url}"
@@ -20,6 +51,8 @@ EOF
 
 	if [ -n "${LIFERAY_CADDY_404_URL}" ]
 	then
+		_log_json "Liferay Caddy is redirecting HTTP 404 responses to ${LIFERAY_CADDY_404_URL}."
+
 		cat >> /etc/caddy.d/liferay_caddy_file << EOF
 handle_errors {
 
@@ -32,7 +65,27 @@ handle_errors {
 EOF
 	fi
 
+	log_configuration
+
 	caddy run --adapter caddyfile --config /etc/caddy/Caddyfile
+}
+
+function _log_json {
+	local escaped_message
+
+	escaped_message=$(echo "${1}" | sed --expression 's/"/\\"/g')
+
+	local script_name
+
+	script_name=$(basename "${0}")
+
+	local severity="${2:-INFO}"
+
+	local timestamp
+
+	timestamp=$(date --utc +"%Y-%m-%dT%H:%M:%SZ")
+
+	printf "{\"message\": \"%s\", \"script\": \"%s\", \"severity\": \"%s\", \"timestamp\": \"%s\"}\n" "${escaped_message}" "${script_name}" "${severity}" "${timestamp}"
 }
 
 main
